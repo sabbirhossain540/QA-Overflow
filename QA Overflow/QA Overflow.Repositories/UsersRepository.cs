0c912d864be000d2366567c23e4fe9bf9cb550a8
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-09-29 17:07:39 +0600

    sdfs

---
+++ QA Overflow/QA Overflow.Repositories/UsersRepository.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QA_Overflow.DomainModels;
+
+namespace QA_Overflow.Repositories
+{
+    public interface IUsersRepository
+    {
+        void InsertUser(User u);
+        void UpdateUserDetails(User u);
+        void UpdateUserPassword(User u);
+        void DeleteUser(int uid);
+        List<User> GetUsers();
+        List<User> GetUsersByEmailAndPassword(string Email, string Password);
+        List<User> GetUsersByEmail(string Email);
+        List<User> GetUsersByUserID(int UserID);
+        int GetLatestUserID();
+    }
+
+
+    public class UsersRepository:IUsersRepository
+    {
+        QAOverflowDatabaseDbContext db;
+
+        public UsersRepository()
+        {
+            db = new QAOverflowDatabaseDbContext();
+        }
+
+        public void InsertUser(User u)
+        {
+            db.Users.Add(u);
+            db.SaveChanges();
+        }
+
+        public void UpdateUserDetails(User u)
+        {
+            User us = db.Users.Where(temp => temp.UserID == u.UserID).FirstOrDefault();
+            if(us != null)
+            {
+                us.Name = u.Name;
+                us.Mobile = u.Mobile;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateUserPassword(User u)
+        {
+            User us = db.Users.Where(temp => temp.UserID == u.UserID).FirstOrDefault();
+            if (us != null)
+            {
+                us.PasswordHash = u.PasswordHash;
+                db.SaveChanges();
+            }
+        }
+
+
+        public void DeleteUser(int uid)
+        {
+            User us = db.Users.Where(temp => temp.UserID == uid).FirstOrDefault();
+            if (us != null)
+            {
+                db.Users.Remove(us);
+                db.SaveChanges();
+            }
+        }
+
+
+        public List<User> GetUsers()
+        {
+            List<User> us = db.Users.Where(temp => temp.IsAdmin == false).OrderBy(temp => temp.Name).ToList();
+            return us;
+        }
+
+
+        public List<User> GetUsersByEmailAndPassword(string Email, string Password)
+        {
+            List<User> us = db.Users.Where(temp => temp.Email == Email && temp.PasswordHash == Password).ToList();
+            return us;
+        }
+
+        public List<User> GetUsersByEmail(string Email)
+        {
+            List<User> us = db.Users.Where(temp => temp.Email == Email).ToList();
+            return us;
+        }
+
+        public List<User> GetUsersByUserID(int UserID)
+        {
+            List<User> us = db.Users.Where(temp => temp.UserID == UserID).ToList();
+            return us;
+        }
+
+        public int GetLatestUserID()
+        {
+            int uid = db.Users.Select(temp => temp.UserID).Max();
+            return uid;
+        }
+    }
+}
