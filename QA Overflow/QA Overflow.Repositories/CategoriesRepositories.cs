c4da09b0a5dd9cea08b0dec5e277dacbb351089a
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-09-29 17:49:36 +0600

    dsfsd

---
+++ QA Overflow/QA Overflow.Repositories/CategoriesRepositories.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QA_Overflow.DomainModels;
+
+namespace QA_Overflow.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        void InsertCategory(Category c);
+        void UpdateCategory(Category c);
+        void DeleteCategory(int cid);
+        List<Category> GetCategories();
+        List<Category> GetCategoryByCategoryID(int CategoryID);
+
+    }
+
+
+    public class CategoriesRepositories:ICategoriesRepository
+    {
+        QAOverflowDatabaseDbContext db;
+
+        public CategoriesRepositories()
+        {
+            db = new QAOverflowDatabaseDbContext();
+        }
+
+        public void InsertCategory(Category c)
+        {
+            db.Categories.Add(c);
+            db.SaveChanges();
+        }
+
+        public void UpdateCategory(Category c)
+        {
+            Category ct = db.Categories.Where(temp => temp.CategoryID == c.CategoryID).FirstOrDefault();
+            if(ct != null)
+            {
+                ct.CategoryName = c.CategoryName;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int cid)
+        {
+            Category ct = db.Categories.Where(temp => temp.CategoryID == cid).FirstOrDefault();
+            if(ct != null)
+            {
+                db.Categories.Remove(ct);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            List<Category> ct = db.Categories.ToList();
+            return ct;
+        }
+
+        public List<Category> GetCategoryByCategoryID(int CategoryID)
+        {
+            List<Category> ct = db.Categories.Where(temp => temp.CategoryID == CategoryID).ToList();
+            return ct;
+        }
+
+    }
+}
